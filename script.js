1047a7407f7f396e3948e164952de15684a9c20a
nerwex02-ctrl <nerwex02@gmail.com>
2026-05-25 02:45:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,139 @@
-let tg = window.Telegram.WebApp;
+let tg =
+window.Telegram.WebApp;
 
 tg.expand();
 
-document.getElementById("name").innerText =
-tg.initDataUnsafe.user?.first_name || "Игрок";
+document
+.getElementById("name")
+.innerText =
+tg.initDataUnsafe.user
+?.first_name || "Игрок";
 
 let lvl = 1;
 
-const items = [
+let hp = 100;
+
+let gold = 0;
+
+const enemies = [
+
+"Слизень",
+"Скелет",
+"Орк",
+"Демон",
+"Дракон"
+
+];
+
+const prefixes = [
+
+"Теневой",
+"Проклятый",
+"Ледяной",
+"Огненный",
+"Древний"
+
+];
+
+const weapons = [
+
+"меч",
+"лук",
+"кинжал",
+"посох",
+"броня"
+
+];
+
+const suffixes = [
+
+"хаоса",
+"смерти",
+"тьмы",
+"вечности",
+"бездны"
+
+];
+
+const rarities = [
 
 {
-name:"Легендарный меч бездны",
-rarity:"legendary"
+name:"common",
+chance:50
 },
 
 {
-name:"Эпический лук теней",
-rarity:"epic"
+name:"rare",
+chance:30
 },
 
 {
-name:"Редкий амулет хаоса",
-rarity:"rare"
+name:"epic",
+chance:15
 },
 
 {
-name:"Необычная броня",
-rarity:"uncommon"
+name:"legendary",
+chance:5
 }
 
 ];
 
-function randomItem(){
+function rarity(){
 
-return items[
-Math.floor(Math.random()*items.length)
+let roll =
+Math.random()*100;
+
+let total = 0;
+
+for(let r of rarities){
+
+total += r.chance;
+
+if(roll <= total){
+
+return r.name;
+
+}
+
+}
+
+return "common";
+
+}
+
+function random(arr){
+
+return arr[
+Math.floor(
+Math.random()*arr.length
+)
 ];
+
+}
+
+function createItem(){
+
+return {
+
+name:
+
+random(prefixes)+" "+
+
+random(weapons)+" "+
+
+random(suffixes),
+
+rarity:rarity()
+
+};
 
 }
 
 function addItem(item){
 
-let div = document.createElement("div");
+let div =
+document.createElement("div");
 
 div.className =
 "item "+item.rarity;
@@ -49,23 +141,71 @@ div.className =
 div.innerHTML =
 "🎁 "+item.name;
 
-document.getElementById("drops")
+document
+.getElementById("drops")
 .prepend(div);
+
+}
+
+function updateUI(){
+
+document
+.getElementById("lvl")
+.innerText = lvl;
+
+document
+.getElementById("hp")
+.innerText = hp;
+
+document
+.getElementById("gold")
+.innerText = gold;
+
+document
+.getElementById("enemy")
+.innerText =
+random(enemies);
 
 }
 
 function fight(){
 
-let item = randomItem();
+let dmg =
+Math.floor(
+Math.random()*20
+);
+
+hp -= dmg;
+
+gold +=
+Math.floor(
+Math.random()*50
+);
+
+let item =
+createItem();
 
 addItem(item);
+
+if(hp <= 0){
+
+alert("💀 Ты умер");
+
+hp = 100;
+
+}
+
+updateUI();
 
 }
 
 function openChest(){
 
-let item = randomItem();
+let item =
+createItem();
 
 addItem(item);
 
 }
+
+updateUI();
